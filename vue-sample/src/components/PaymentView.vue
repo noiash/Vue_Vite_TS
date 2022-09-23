91753d44a4896df0e8906a97b51823f6e0bf9c2f
noiash <noinoi51267@gmail.com>
2022-09-23 11:10:33 +0900

    [Start]Vue3_TS

---
+++ vue-sample/src/components/PaymentView.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+// const itemName1 = ref<string>('Desk')
+const itemName2 = 'Bike'
+
+const item1 = reactive({
+  name: 'Desk',
+  price: 40000
+})
+
+// const price1 = 40000
+const price2 = 20000
+
+const url1 = 'https://www.aboutamazon.jp/'
+
+const buy = (itemName: string) => {
+  alert('Are you sure to buy ' + itemName + '?')
+}
+
+const input = (event: any) => {
+  item1.name = event.target.value
+}
+const inputPrice = (event: any) => {
+  item1.price = event.target.value
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Payment</h1>
+    <input @input="input"/>
+    <input @input="inputPrice"/>
+    <div class="payment">
+      <label>{{ item1.name }}</label>
+      <label>{{ item1.price }}yen</label>
+      <a :href="url1">bought at...</a>
+      <button @click="buy(item1.name)">BUY</button>
+    </div>
+    <div class="payment">
+      <label>{{ itemName2 }}</label>
+      <label>{{ price2 }}yen</label>
+      <button @click="buy(itemName2)">BUY</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .payment {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    width: 400px;
+    background-color: aliceblue;
+    margin-bottom: 8px;
+  }
+
+  input {
+    margin-bottom: 8px;
+  }
+  label {
+    font-size: 20px;
+    font-weight: bold;
+  }
+</style>
